1cdf7f80397fb4551cb306e431f648610b6ce0a8
Gasser Elnaggar <140742019+Gasser5050@users.noreply.github.com>
2026-06-20 14:16:49 +0300

    improved footer accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="flex justify-between py-2 mt-10 bg-[hsl(200,100%,10%)]">
       <p className="text-[hsl(200,20%,95%)] pl-3">&copy;MyApp</p>
-      <p className="text-slate-500 pr-3">
+      <p className="text-[hsl(200,20%,95%)] pr-3">
         This Project uses JSONPlaceholder Api
       </p>
     </footer>
